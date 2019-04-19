97e3d714d8c3e88e7c84314e10d7ef4d4b426c5b
Unknown <berthetmathieu@free.fr>
2019-04-19 17:21:43 +0200

    Add deplacement for player

---
+++ Assets/Script/Deplacement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Deplacement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public NavMeshAgent navMeshPlayer;
+    public RaycastHit hit;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        navMeshPlayer = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            hit = new RaycastHit();
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out hit))
+            {
+                    navMeshPlayer.destination = hit.point;
+            }
+        }
+    }
+}
---
+++ Assets/Script/StockOfToken.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StockOfToken : MonoBehaviour
+{
+    public int nbRedToken = 4;
+    public int nbGreenToken = 4;
+    public int nbBlueToken = 4;
+    public int nbYellowToken = 4;
+    public int nbBlackToken = 4;
+    public int yinYangRedToken = 0;
+    public int yinYangGreenToken = 0;
+    public int yinYangBlueToken = 0;
+    public int yinYangYellowToken = 0;
+    public int nbPowerToken = 3;
+}
